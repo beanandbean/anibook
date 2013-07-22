67b8537175ad374f29435cff37a1598f1b27f4c2
codingpotato <codingpotato@gmail.com>
2013-07-22 19:46:47 +0800

    Initialize project

---
+++ src/anibook/anibook/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  anibook
+//
+//  Created by wangyw on 7/21/13.
+//  Copyright (c) 2013 codingpotato. All rights reserved.
+//
+
+#import "CPAppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPAppDelegate class]));
+    }
+}
