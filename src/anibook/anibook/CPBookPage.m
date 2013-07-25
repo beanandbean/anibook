3ecffb2cdcf9ca98eb9d274d4eb72f483586eeab
codingpotato <codingpotato@gmail.com>
2013-07-26 07:22:03 +0800

    Add audio support

--- src/anibook/anibook/CPBookPage.m
+++ src/anibook/anibook/CPBookPage.m
@@ -14,6 +14,7 @@
 
 #import "CCBReader.h"
 #import "CCBAnimationManager.h"
+#import "SimpleAudioEngine.h"
 
 @interface CPBookPage ()
 
@@ -31,6 +32,8 @@
     CCScene* scene = [CCBReader sceneWithNodeGraphFromFile:@"BookShelf.ccbi" owner:self];
     
     [[CCDirector sharedDirector] replaceScene:[CCTransitionFlipX transitionWithDuration:1.0 scene:scene]];
+    
+    [[SimpleAudioEngine sharedEngine] playEffect:@"ButtonSound.caf"];
 }
 
 - (void)pressedNext:(id)sender {
